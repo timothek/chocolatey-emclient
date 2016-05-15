8d76fb507dc9c9014a9c62805c66ea13b06b8c30
timothek <timothek@icloud.com>
2016-05-15 21:52:02 +0200

    Added the nuspec-file, an icon and an install script.

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName= 'em-client' # arbitrary name for the package, used in messages
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://de.emclient.com/dist/latest/setup.msi' # download url
+
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'MSI'
+  url           = $url
+
+  silentArgs    = "/qn /norestart /l*v `"$env:TEMP\chocolatey\$($packageName)\$($packageName).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+
+  softwareName  = 'em client*'
+}
+
+Install-ChocolateyPackage @packageArgs
