6add187cd95dca6af311fcdaeff396863512296f
timothek <timothek@icloud.com>
2016-05-16 15:37:24 +0200

    Deleted wrong "/" in $silentArgs

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $packageName	= 'em-client'
 $fileType		= 'MSI'
 $32BitUrl		= 'http://de.emclient.com/dist/latest/setup.msi'
 $64BitUrl		= $32BitUrl
-$silentArgs		= "/qn /norestart /"
+$silentArgs		= "/qn /norestart"
 $validExitCodes	= @(0, 3010, 1641)
 
 # install eM Client
