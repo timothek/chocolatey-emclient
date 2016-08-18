5ce577ad3a67894f2f8214623a14eec7d1b3dc27
timothek <timothek@icloud.com>
2016-08-18 18:13:51 +0200

    Updated the package to install a specific Version of eM Client.
    The package now installs version 7.0.26687 of eM Client. I also added the checksum, as chocolatey now requires this for HTTP downloads.

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,10 +1,20 @@
-﻿
-$packageName	= 'em-client'
+﻿$packageName	= 'em-client'
 $fileType		= 'MSI'
-$32BitUrl		= 'http://de.emclient.com/dist/latest/setup.msi'
-$64BitUrl		= $32BitUrl
+$url			= 'http://www.emclient.com/dist/v7.0.26687/setup.msi'
 $silentArgs		= "/qn /norestart"
 $validExitCodes	= @(0, 3010, 1641)
+$checksum  		= '7408A501D12C43117611C164CED6C5B9149CC22F5AD394DD0DC370B190C8FD70'
 
-# install eM Client
-Install-ChocolateyPackage "$packageName" "$fileType" "$silentArgs" "$32BitUrl" "$64BitUrl" -validExitCodes $validExitCodes
+$packageArgs = @{
+  packageName            = $packageName
+  fileType               = $fileType
+  url                    = $url
+  url64					 = $url
+  silentArgs             = $silentArgs
+  checksum               = $checksum
+  checksum64             = $checksum
+  checksumType           = 'sha256'
+  checksumType64         = 'sha256'
+  validExitCodes		 = @(0, 3010, 1641)
+}
+Install-ChocolateyPackage @packageArgs
