aede490ab515e322231ac9d3b2f6bf0ebc311255
timothek <timothek@icloud.com>
2016-08-18 18:18:48 +0200

    Fixed tabs on chocolateyinstall.ps1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -6,15 +6,15 @@ $validExitCodes	= @(0, 3010, 1641)
 $checksum  		= '7408A501D12C43117611C164CED6C5B9149CC22F5AD394DD0DC370B190C8FD70'
 
 $packageArgs = @{
-  packageName            = $packageName
-  fileType               = $fileType
-  url                    = $url
-  url64					 = $url
-  silentArgs             = $silentArgs
-  checksum               = $checksum
-  checksum64             = $checksum
-  checksumType           = 'sha256'
-  checksumType64         = 'sha256'
-  validExitCodes		 = @(0, 3010, 1641)
+  packageName           = $packageName
+  fileType              = $fileType
+  url                   = $url
+  url64                 = $url
+  silentArgs            = $silentArgs
+  checksum              = $checksum
+  checksum64            = $checksum
+  checksumType          = 'sha256'
+  checksumType64        = 'sha256'
+  validExitCodes        = @(0, 3010, 1641)
 }
 Install-ChocolateyPackage @packageArgs
