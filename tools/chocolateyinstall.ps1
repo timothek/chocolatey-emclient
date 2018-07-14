fca3cdf2b6f0a32258c5de0d7088a6351a6499a9
timothek <timo.kluge@nordakademie.org>
2018-07-14 20:59:08 +0200

    Update to eM Client 7.1.33101

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 ﻿$packageName	= 'em-client'
 $fileType		= 'MSI'
-$url			= 'http://www.emclient.com/dist/v7.1.31849/setup.msi'
+$url			= 'http://www.emclient.com/dist/v7.1.33101/setup.msi'
 $silentArgs		= "/qn /norestart"
 $validExitCodes	= @(0, 3010, 1641)
-$checksum  		= 'f5dd2720cb57e8ed167128c285523388c7e1c144d755051e06371ad5aaf623bb'
+$checksum  		= '0b37ec9be2a0ea08baa87bfcd5ef280f9d6cce80d10d781b59e4ed25ce6767ce'
 
 $packageArgs = @{
   packageName           = $packageName
