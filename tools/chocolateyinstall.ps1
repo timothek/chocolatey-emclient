5f1cb92cffbae9bed317b3fb580d329f7592f568
Timo <gitea-timo@kluge.space>
2021-10-13 19:32:26 +0200

    Update to eM Client 8.2.1659

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 ﻿$packageName	= 'em-client'
 $fileType		= 'MSI'
-$url			= 'https://www.emclient.com/dist/v8.2.1237/setup.msi'
+$url			= 'https://www.emclient.com/dist/v8.2.1659/setup.msi'
 $silentArgs		= "/qn /norestart"
 $validExitCodes	= @(0, 3010, 1641)
-$checksum  		= 'ce87340237cf4ff81211482c8f52195132520dde8ccd17a6414bf2aaa9397436'
+$checksum  		= 'b5ede8239e451b43158204924f78d849de29f6110390ab8d4020428daa627db6'
 
 $packageArgs = @{
   packageName           = $packageName
