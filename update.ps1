559ccec7d841762c2352804944c7c3d78d08b2d9
Timo <gitea-timo@kluge.space>
2022-01-13 21:09:37 +0100

    added github actions

--- update.ps1
+++ update.ps1
@@ -15,8 +15,8 @@ function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases
 
     $re      = 'setup.msi'
-    $url     = $download_page.links | ? href -match $re | select -First 1 -expand href
-    $version = ($url -split "/" | select -Index 4).Substring(1)
+    $url     = $download_page.links | Where-Object href -match $re | Select-Object -First 1 -expand href
+    $version = ($url -split "/" | Select-Object -Index 4).Substring(1)
 
     return @{ URL32 = $url; Version = $version; ChecksumType32 = 'sha256';}
 }
