54eeee3a438fb29084af45c5aca16ae286955772
Timo <gitea-timo@kluge.space>
2022-01-27 23:43:31 +0100

    Triggering a workflow from a workflow 1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 ﻿$packageName	= 'em-client'
 $fileType		= 'MSI'
-$url			= 'https://www.emclient.com/dist/v9.0.790/setup.msi'
+$url			= 'https://www.emclient.com/dist/v9.0.599/setup.msi'
 $silentArgs		= "/qn /norestart"
 $validExitCodes	= @(0, 3010, 1641)
-$checksum  		= '822887642a53a67d030d4696e0e966d517bd18becce183978263491f164d61c5'
+$checksum  		= '6c5403114d7c8a6361e865502d75d9dbf14e27a2a7dc60a2ce1f04154d50cf47'
 
 $packageArgs = @{
   packageName           = $packageName
